73c9bf9c5fc09a596cbe4951f29256b2b4edd45d
LNine <nine-little@outlook.com>
2025-04-02 15:39:37 +0800

    .

--- examples/lightrag_openai_compatible_demo.py
+++ examples/lightrag_openai_compatible_demo.py
@@ -5,11 +5,8 @@ from lightrag.llm.openai import openai_complete_if_cache, openai_embed
 from lightrag.utils import EmbeddingFunc
 import numpy as np
 from lightrag.kg.shared_storage import initialize_pipeline_status
-import json
 
-import datetime
-
-WORKING_DIR = "./ragtest"
+WORKING_DIR = "./dickens"
 
 if not os.path.exists(WORKING_DIR):
     os.mkdir(WORKING_DIR)
@@ -19,12 +16,12 @@ async def llm_model_func(
     prompt, system_prompt=None, history_messages=[], keyword_extraction=False, **kwargs
 ) -> str:
     return await openai_complete_if_cache(
-        "glm-4-air",
+        "solar-mini",
         prompt,
         system_prompt=system_prompt,
         history_messages=history_messages,
-        api_key=os.getenv("ZHIPUAI_API_KEY"),
-        base_url="https://open.bigmodel.cn/api/paas/v4",
+        api_key=os.getenv("UPSTAGE_API_KEY"),
+        base_url="https://api.upstage.ai/v1/solar",
         **kwargs,
     )
 
@@ -32,9 +29,9 @@ async def llm_model_func(
 async def embedding_func(texts: list[str]) -> np.ndarray:
     return await openai_embed(
         texts,
-        model="embedding-3",
-        api_key=os.getenv("ZHIPUAI_API_KEY"),
-        base_url="https://open.bigmodel.cn/api/paas/v4",
+        model="solar-embedding-1-large-query",
+        api_key=os.getenv("UPSTAGE_API_KEY"),
+        base_url="https://api.upstage.ai/v1/solar",
     )
 
 
@@ -52,6 +49,7 @@ async def test_funcs():
 
     result = await embedding_func(["How are you?"])
     print("embedding_func: ", result)
+
 
 # asyncio.run(test_funcs())
 
@@ -78,59 +76,41 @@ async def initialize_rag():
 
 async def main():
     try:
-
-        start_time = datetime.datetime.now()
-
         # Initialize RAG instance
         rag = await initialize_rag()
 
-        with open("./ragtest/test.json", "r", encoding="utf-8") as f:
-            data = json.load(f)
-
-        with open("./input/新管控系统操作手册.txt", "r", encoding="utf-8") as f:
+        with open("./book.txt", "r", encoding="utf-8") as f:
             await rag.ainsert(f.read())
-            end_time = datetime.datetime.now()
-            duration = (end_time - start_time).total_seconds()
-            data.insert(0, {"insertion_time": f"{duration:.3f}秒"})
-            print("构建完成")
-        
 
+        # Perform naive search
+        print(
+            await rag.aquery(
+                "What are the top themes in this story?", param=QueryParam(mode="naive")
+            )
+        )
 
-        for item in data:
+        # Perform local search
+        print(
+            await rag.aquery(
+                "What are the top themes in this story?", param=QueryParam(mode="local")
+            )
+        )
 
-            if data.index(item) == 0:
-                continue
+        # Perform global search
+        print(
+            await rag.aquery(
+                "What are the top themes in this story?",
+                param=QueryParam(mode="global"),
+            )
+        )
 
-            question = item["question"]
-
-            start_time = datetime.datetime.now()
-            item["naive"] = await rag.aquery(question, param=QueryParam(mode="naive"))
-            end_time = datetime.datetime.now()
-            item["naive_query_time"] = f"{(end_time - start_time).total_seconds():.3f}秒"
-
-            start_time = datetime.datetime.now()
-            item["local"] = await rag.aquery(question, param=QueryParam(mode="local"))
-            end_time = datetime.datetime.now()
-            item["local_query_time"] = f"{(end_time - start_time).total_seconds():.3f}秒"
-
-            start_time = datetime.datetime.now()
-            item["global"] = await rag.aquery(question, param=QueryParam(mode="global"))
-            end_time = datetime.datetime.now()
-            item["global_query_time"] = f"{(end_time - start_time).total_seconds():.3f}秒"
-
-            start_time = datetime.datetime.now()
-            item["hybrid"] = await rag.aquery(question, param=QueryParam(mode="hybrid"))
-            end_time = datetime.datetime.now()
-            item["hybrid_query_time"] = f"{(end_time - start_time).total_seconds():.3f}秒"
-
-            start_time = datetime.datetime.now()
-            item["mix"] = await rag.aquery(question, param=QueryParam(mode="mix"))
-            end_time = datetime.datetime.now()
-            item["mix_query_time"] = f"{(end_time - start_time).total_seconds():.3f}秒"
-
-        with open("./ragtest/test.json", "w", encoding="utf-8") as f:
-            json.dump(data, f, ensure_ascii=False, indent=4)
-        
+        # Perform hybrid search
+        print(
+            await rag.aquery(
+                "What are the top themes in this story?",
+                param=QueryParam(mode="hybrid"),
+            )
+        )
     except Exception as e:
         print(f"An error occurred: {e}")
 
